13220be0b29954960af969c33517f68da49b660f
vinu-prasad-e1936 <vinu.prasad@freshworks.com>
2021-08-15 22:12:59 +0530

    Initial  commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import Badge from "react-bootstrap/Badge";
 import './App.css';
+import { useState } from 'react';
+let marked = require("marked");
 
-function App() {
+const App = () => {
+  const [markdown, setMarkdown] = useState('');
+
+  const onChangeHandler = (event) => {
+    console.log(event.target.value);
+    setMarkdown(event.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col text-center">
+            <h1>
+              <Badge className="text-align-center" variant="light">
+                Markdown Previewer
+                </Badge>
+            </h1>
+          </div>
+        </div>
+        <div className="row mt-4">
+          <div className="col-md-6">
+            <div className="col text-center">
+              <h4>
+                <Badge className="text-align-center" variant="secondary">
+                  Markdown Input
+                  </Badge>
+              </h4>
+              <div className="mark-input inputStyle">
+                <textarea value={markdown} onChange={(event) => onChangeHandler(event)} className="input inputStyle"/>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="col text-center">
+              <h4>
+                <Badge className="text-align-center" variant="secondary">
+                  Preview
+                  </Badge>
+              </h4>
+            </div>
+            <div className="outputStyle" dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+></div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
 export default App;
