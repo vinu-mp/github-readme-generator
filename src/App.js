4fe47c0f7852a105b65b7110d5db997103b3995c
vinu-mp <mp.prasadvinu@gmail.com>
2021-08-16 23:41:05 +0530

    Update design initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import Badge from "react-bootstrap/Badge";
 import './App.css';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 import { useState } from 'react';
+
 let marked = require("marked");
 
 const App = () => {
@@ -10,45 +12,74 @@ const App = () => {
     console.log(event.target.value);
     setMarkdown(event.target.value);
   }
+
   return (
-    <div className="App">
-      <div className="container">
-        <div className="row mt-4">
-          <div className="col text-center">
-            <h1>
-              <Badge className="text-align-center" variant="light">
-                Markdown Previewer
-              </Badge>
-            </h1>
+    <div className="readme">
+      <header className="header">
+        <div className="header_logo">Readme</div>
+      </header>
+      <main className="rg_container h-100">
+        <div className="row m-0 h-100">
+          <div className="monaco_wrapper col-md-6">
+          </div>
+          <div className="preview_wrapper col-md-6">
           </div>
         </div>
-        <div className="row mt-4">
-          <div className="col-md-6">
-            <div className="col text-center">
-              <h4>
-                <Badge className="text-align-center" variant="secondary">
-                  Markdown Input
-                </Badge>
-              </h4>
-              <div className="mark-input inputStyle">
-                <textarea value={markdown} onChange={(event) => onChangeHandler(event)} className="input inputStyle" />
-              </div>
+      </main>
+      <footer>
+
+      </footer>
+      {/* <Header/>
+        <main className="readme_inner">
+          <div className="row">
+            <div className="monaco_wrapper col-md-6">
+
+            </div>
+            <div className="preview_wrapper col-md-6">
+
             </div>
           </div>
-          <div className="col-md-6">
-            <div className="col text-center">
-              <h4>
-                <Badge className="text-align-center" variant="secondary">
-                  Preview
-                </Badge>
-              </h4>
-            </div>
-            <div className="outputStyle" dangerouslySetInnerHTML={{ __html: marked(markdown) }}
-            ></div>
-          </div>
-        </div>
-      </div>
+        </main>
+      <Footer/> */}
     </div>
+    // <div className="App">
+    //   <div className="container">
+    //     <div className="row mt-4">
+    //       <div className="col text-center">
+    //         <h1>
+    //           <Badge className="text-align-center" variant="light">
+    //             Markdown Previewer
+    //           </Badge>
+    //         </h1>
+    //       </div>
+    //     </div>
+    //     <div className="row mt-4">
+    //       <div className="col-md-6">
+    //         <div className="col text-center">
+    //           <h4>
+    //             <Badge className="text-align-center" variant="secondary">
+    //               Markdown Input
+    //             </Badge>
+    //           </h4>
+    //           <div className="mark-input inputStyle">
+    //             <textarea value={markdown} onChange={(event) => onChangeHandler(event)} className="input inputStyle" />
+    //           </div>
+    //         </div>
+    //       </div>
+    //       <div className="col-md-6">
+    //         <div className="col text-center">
+    //           <h4>
+    //             <Badge className="text-align-center" variant="secondary">
+    //               Preview
+    //             </Badge>
+    //           </h4>
+    //         </div>
+    //         <div className="outputStyle" dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+    //         ></div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 export default App;
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import HOC from '../HOC/HOC';
+
+const Footer = (props) => {
+  return (
+
+    <footer class="folio__footer l-container">
+      <HOC>
+          @2021 mp.prasadvinu
+      </HOC>
+    </footer>
+  )
+}
+
+export default Footer;
