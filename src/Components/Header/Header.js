170bb16238b0a3588facc373f6a1fe312976a35c
vinu-mp <mp.prasadvinu@gmail.com>
2021-11-17 10:30:18 +0530

    Add favicon and hamburger menu

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,27 @@
 import styles from './Header.module.scss';
 import Logo from '../../assets/images/w-edit-logo.png';
+import { useState } from 'react';
 
 const Header = () => {
+  const [isBurgerMenuActive, setBurgerMenuActive] = useState(false);
+
+  const toggleNav = () => {
+    setBurgerMenuActive((prev) => !prev)
+  }
+
   return (
     <header className={styles.header}>
-      <nav className={styles.header_nav}>
-        <a className={styles.header_logo} href="#">
+      <nav className={styles.header__nav}>
+        <a className={styles.header__logo} href="#">
           <img src={Logo} alt="logo"/>
         </a>
-        <ul className='mb-0'>
-          <li><a href="/download" className={styles['header_nav-links']}>Download</a></li>
-          <li><a href="/about" className={styles['header_nav-links']}>About</a></li>
-        </ul>
+        <div className='row m-0'>
+          <span onClick={toggleNav} className={`d-md-none ${styles['header__burger-menu']} ${isBurgerMenuActive ? styles.active : ''}`}></span>
+          <ul className='mb-0 d-none d-md-flex'>
+            <li><a href="/download" className={styles['header__nav-links']}>Download</a></li>
+            <li><a href="/about" className={styles['header__nav-links']}>About</a></li>
+          </ul>
+        </div>
       </nav>
     </header>
   )
