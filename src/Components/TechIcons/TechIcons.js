5c2f8fb46df7af83af89bea8815ea6fff033945a
vinu-mp <mp.prasadvinu@gmail.com>
2023-03-26 08:37:35 +0530

    Rename folder

--- src/Components/TechIcons/TechIcons.js
+++ src/Components/TechIcons/TechIcons.js
@@ -1,10 +1,8 @@
-import IconWrapper from '../UI/IconWrapper/IconWrapper';
-import techIcons from '../../data/icons.json';
+import IconWrapper from "../UI/IconWrapper/IconWrapper";
+import techIcons from "../../Data/icons.json";
 
 const TechIcons = () => {
-  return (
-    <IconWrapper isIcon={true} data={techIcons} />
-  )
-}
+  return <IconWrapper isIcon={true} data={techIcons} />;
+};
 
 export default TechIcons;
